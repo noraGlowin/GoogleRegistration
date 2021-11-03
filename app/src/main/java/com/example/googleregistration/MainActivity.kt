453b1d1ff8ab3f9bf0689b059a210523406f5a4f
Nora <Norah.Nahedh.ruh.k@tuwaiq.edu.sa>
2021-11-03 09:28:54 +0300

    fix bug , thanks Mr Hussain

--- app/src/main/java/com/example/googleregistration/MainActivity.kt
+++ app/src/main/java/com/example/googleregistration/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.googleregistration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.googleregistration.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,22 +16,22 @@ class MainActivity : AppCompatActivity() {
     fun registration() {
         val firstName = binding.firstName.text.toString()
         val lastName = binding.lastName.text.toString()
-        val email = binding.useEmail.text.toString()
+        val email = binding.userName.text.toString()
+
         val password =binding.password.text.toString()
         val confirm = binding.confirm.text.toString()
 
 if (firstName.isNotEmpty()&& lastName.isNotEmpty()&& email.isNotEmpty() &&password.isNotEmpty()&&confirm.isNotEmpty()){
+    //Toast.makeText(MainActivity@ this, "in $email ", Toast.LENGTH_SHORT).show()
     if (email.contains('!') || email.contains('@')||email.contains(' ') || email.contains('#')
         || email.contains('$')||email.contains('%') || email.contains('^')
         || email.contains('&')||email.contains('*') || email.contains('?')
-        || email.contains('>')||email.contains('-') || email.contains('_')
-
-
-    ){
+        || email.contains('>')||email.contains('-') || email.contains('_')){
+        Toast.makeText(MainActivity@ this, "in if ", Toast.LENGTH_SHORT).show()
           binding.result.setText("Please Enter User Name Without Symbol or spaces")
     }
-    if (password==confirm){
-        binding.result.setText("Hi $firstName you register successfully")
+   else if (password==confirm){
+       binding.result.setText("Hi $firstName you register successfully")
     }else{
         binding.result.setText("Password Missmatch")
     }
